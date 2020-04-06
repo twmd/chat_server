dee12653eed3a8b47486f5ad2f681472e04da3fd
twmd <slava.sazanov@gmail.com>
2020-04-06 18:10:32 +0300

    test

--- server.py
+++ server.py
@@ -7,34 +7,49 @@ import lib.common as common
 # Получить адресс и порт из функции
 address, port = common.getting_arguments()
 server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=0)
-chat_client = []
 # Вызывает exception если адресс или порт указанны неверно.
 try:
     server_socket.bind((address, port))
 except OSError:
     print('Неправельно указан адрес или порт')
 server_socket.listen(100)
-while True:
-    sock, addr = server_socket.accept()
-    # Создаем список сокетов
-    chat_client.append(sock)
-    # Очищаем стандартный список после прохождение цикла
-    soc_client_r = []
-    soc_client_w = []
-    # присваеваем списку, активных клиентов что могут читать и писать
-    _ , soc_client_w, _ = select.select([], chat_client, [], 1)
-    # проходимся по списку что нам что то прислали
-    for r_client in soc_client_w:
-        print('r_client = {}'.format(r_client))
-        try:
-            # Тест для вывода сообщений сервера
-            data = libsrv.get_data_from_socket(r_client)
+chat_client_in = server_socket
+chat_client_out = []
+
+while chat_client_in:
+    soc_client_r, soc_client_w, soc_client_e = select.select(chat_client_in, chat_client_out, chat_client_in, 1)
+    for s in soc_client_r:
+        if s is server_socket:
+            sock, addr = s.accept()
+            chat_client_in.append(sock)
+        else:
+            data = libsrv.get_data_from_socket(s)
             if data.get('action') == 'msg':
                 print(data.get('message'))
-                # for w_client in soc_client_w:
-                #     print(data.get('message'))
-                    # libsrv.send_message_all_in_chat(w_client, data.get('message'))
-        except:
-            # удаляем если вернулась ошибка из общего списка
-            chat_client.remove(r_client)
-            print('error')
+                if s not in chat_client_out:
+                    chat_client_out.append(s)
+
+# while True:
+#     sock, addr = server_socket.accept()
+#     # Создаем список сокетов
+#     chat_client.append(sock)
+#     # Очищаем стандартный список после прохождение цикла
+#     soc_client_r = []
+#     soc_client_w = []
+#     # присваеваем списку, активных клиентов что могут читать и писать
+#     _ , soc_client_w, _ = select.select([], chat_client, [], 1)
+#     # проходимся по списку что нам что то прислали
+#     for r_client in soc_client_w:
+#         print('r_client = {}'.format(r_client))
+#         try:
+#             # Тест для вывода сообщений сервера
+#             data = libsrv.get_data_from_socket(r_client)
+#             if data.get('action') == 'msg':
+#                 print(data.get('message'))
+#                 # for w_client in soc_client_w:
+#                 #     print(data.get('message'))
+#                     # libsrv.send_message_all_in_chat(w_client, data.get('message'))
+#         except:
+#             # удаляем если вернулась ошибка из общего списка
+#             chat_client.remove(r_client)
+#             print('error')
